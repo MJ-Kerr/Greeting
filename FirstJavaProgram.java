510a9bbda90b353de28f1350325d0b0f14e56f06
Miles Kerr <m.kerr.professional@gmail.com>
2023-06-05 15:08:00 -0500

    JAVA is here

---
+++ FirstJavaProgram.java
@@ -0,0 +1,9 @@
+import java.lang.constant.Constable;
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Miles Kerr");
+        System.out.println("I'm 38 years old");
+        System.out.println("My hometown is Corpus Christi, TX");
+    }
+}
